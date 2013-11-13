01813e7b8276e0882af82487bedd9595169b8eef
lehins <lehins@yandex.ru>
2013-11-13 11:08:34 -0700

    changes

--- tigerline/admin.py
+++ tigerline/admin.py
@@ -8,15 +8,15 @@ from tigerline.models import State, County, SubCounty
 #    search_fields = ('code',)
 
 
-class StateAdmin(admin.OSMGeoAdmin):
-    list_display = ('name', 'usps_code', 'fips_code')
+#class StateAdmin(admin.OSMGeoAdmin):
+#    list_display = ('name', 'usps_code', 'fips_code')
 
 
-class CountyAdmin(admin.OSMGeoAdmin):
-    list_display = ('id', 'name', 'legal_statistical_description', 'fips_55_class_code', 'functional_status')
-    search_fields = ('name', 'state_fips_code')
+#class CountyAdmin(admin.OSMGeoAdmin):
+#    list_display = ('id', 'name', 'legal_statistical_description', 'fips_55_class_code', 'functional_status')
+#    search_fields = ('name', 'state_fips_code')
 
 
 #admin.site.register(Zipcode, ZipcodeAdmin)
-admin.site.register(State, StateAdmin)
-admin.site.register(County, CountyAdmin)
+#admin.site.register(State, StateAdmin)
+#admin.site.register(County, CountyAdmin)
